79b5fe4dbfb92dd60f499e7aa23f847fecc44038
fellipetereska <fellipetereska@gmail.com>
2025-09-30 14:55:13 -0300

    att logevento

--- api/src/server.ts
+++ api/src/server.ts
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
   const start = Date.now();
   res.on("finish", () => {
     const duration = Date.now() - start;
-    if (duration > 500) console.log(`${req.method} ${req.url} - ${duration}ms`);
+    console.log(`${req.method} ${req.url} - ${duration}ms`);
   });
   next();
 });
